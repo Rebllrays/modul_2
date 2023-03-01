25147c5e570138be141c62b6c93c534af918b48a
Medina <medialieva04@gmail.com>
2023-03-01 14:32:49 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Modules/Navbar';
+import AuthContextProvider from './contexts/authContext';
+import Routing from './routes/Routing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <BrowserRouter>
+  <AuthContextProvider>
+  <Navbar/>
+  <Routing/>
+  </AuthContextProvider>
+  </BrowserRouter>
+  )
 }
 
 export default App;
